029f98fbc0d8bc9040c7a3116bec4842406b169b
Nwbyyy <nixonshowalter@yahoo.com>
2023-08-17 10:04:56 -0700

    try/except for file creation

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Keep track of your savings :)
 - [ ] Get API access (harder than i thought :C)
 - [ ] If i cant, rework project to have manual data entry instead (lame and cringe)
 - [ ] Read file off select location in desktop contain sensitive keys as to not upload them to github :sweat:
-- [ ] Have a seperate file that keeps track of money accounted for
+- [x] Have a seperate file that keeps track of money accounted for
 - [ ] Decide whether you want to sort by transaction or by total money saved or both
 - [ ] Ability to determine balance in savings and designated money into specific categories
 - [ ] Manage money coming in and going out
--- main.py
+++ main.py
@@ -1,11 +1,19 @@
-import os #file path "C:\Program Files\Savings-Docs"
+import os #file path C:\Users\nixon\Documents\laptop backup.zip"
 
-path = os.path.join("C:/Program Files/","Savings-Docs")
+path = os.path.join(os.path.expanduser('~') + "/Documents/","Savings-Docs")
 
 try:
     os.mkdir(path)
-    print("Creating mnew directory...")
+    print("Creating new directory...")
 except OSError as error:
     print("Accessing existing directory...")
 
-print(path)
+try:
+    open(path + "/savings-log.txt", "x")
+    print("Creating new file...")
+    log = (path + "/savings-log.txt", "w")
+    log.writelines(["Balance: ","Categories: ","Unsorted Funds: "])
+except (OSError, IOError) as error:
+    print("Accessing existing file...")
+    
+file_path = path + "/savings-log.txt"
